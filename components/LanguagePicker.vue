05c79235f036e6c07ec90f95eeb89398cf4dac67
Adelino Ngomacha <Adelinob335@gmail.com>
2021-08-12 18:32:30 +0200

    give more live to the site

--- components/LanguagePicker.vue
+++ components/LanguagePicker.vue
@@ -23,18 +23,21 @@
         <button
           class="border border-blue-500 w-16 h-16 rounded-full flex items-center justify-center bg-blue-500 text-white">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"/>
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"/>
           </svg>
         </button>
       </div>
 
       <div class="w-full flex py-4 px-8 relative -mt-8">
-        <div class="border border-gray-300 w-1/2 -mr-1 flex items-center px-4 rounded-l-full" style="border-radius: 50px 60px 0 50px;">
+        <div class="border border-gray-300 w-1/2 -mr-1 flex items-center px-4 rounded-l-full"
+             style="border-radius: 50px 60px 0 50px;">
           <span class="text-blue-500 mr-2 border-b border-blue-500 pb-3 -mb-2">Detect Language</span>
           <span v-for="item in languages.slice(0, 3)" class="text-gray-300 mx-2 pb-1">{{ item }}</span>
         </div>
         <div id="curved-corner-bottomleft"></div>
-        <div class="border border-gray-300 w-1/2 -ml-1 flex items-center px-4 rounded-l-full" style="border-radius: 57px 50px 50px 0;">
+        <div class="border border-gray-300 w-1/2 -ml-1 flex items-center px-4 rounded-l-full"
+             style="border-radius: 57px 50px 50px 0;">
           <span
             v-for="(item, index) in languages.slice(0, 4)"
             :class="{'text-gray-300 pb-1': index !== 0, 'text-blue-500 border-b border-blue-500 pb-3 -mb-2': index === 0}"
@@ -48,31 +51,33 @@
 
       <div class="px-8 font-normal">
         <div class="w-full flex rounded-xl border border-gray-300">
-          <div class="w-1/2 relative">
-            <textarea class="w-full mb-10 textarea--disable-resize textarea--height py-2 text-gray-500"></textarea>
-            <div class="w-full absolute bottom-0 h-16 flex items-center justify-between px-6">
+          <div class="w-1/2 relative px-6">
+            <textarea class="w-full mb-12 textarea--disable-resize textarea--height py-2 text-gray-500 focus:border-transparent focus:outline-none"
+                      :class="textareClassNames" v-model="textareaValue"></textarea>
+            <div class="w-full absolute bottom-0 h-16 flex items-center justify-between -ml-6 px-6">
               <div class="flex items-center text-gray-400">
                 <button>
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6 hover:text-blue-500">
                     <g fill="none">
                       <path d="M0 0h24v24H0z"/>
                       <path d="M0 0h24v24H0z"/>
                       <path d="M0 0h24v24H0z"/>
                     </g>
-                    <path fill="currentColor" d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z"/>
                     <path fill="currentColor"
-                      d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z"/>
+                          d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z"/>
+                    <path fill="currentColor"
+                          d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z"/>
                   </svg>
                 </button>
                 <button class="mx-4">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6 hover:text-blue-500">
                     <path d="M0 0h24v24H0V0z" fill="none"/>
                     <path fill="currentColor"
-                      d="M3 9v6h4l5 5V4L7 9H3zm7-.17v6.34L7.83 13H5v-2h2.83L10 8.83zM16.5 12A4.5 4.5 0 0014 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77 0-4.28-2.99-7.86-7-8.77z"/>
+                          d="M3 9v6h4l5 5V4L7 9H3zm7-.17v6.34L7.83 13H5v-2h2.83L10 8.83zM16.5 12A4.5 4.5 0 0014 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77 0-4.28-2.99-7.86-7-8.77z"/>
                   </svg>
                 </button>
                 <button>
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6 fill-current">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6 fill-current hover:text-blue-500">
                     <path
                       d="M20 5H4c-1.1 0-1.99.9-1.99 2L2 17c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-9 3h2v2h-2V8zm0 3h2v2h-2v-2zM8 8h2v2H8V8zm0 3h2v2H8v-2zm-1 2H5v-2h2v2zm0-3H5V8h2v2zm9 7H8v-2h8v2zm0-4h-2v-2h2v2zm0-3h-2V8h2v2zm3 3h-2v-2h2v2zm0-3h-2V8h2v2z"/>
                     <path d="M0 0h24v24H0zm0 0h24v24H0z" fill="none"/>
@@ -80,13 +85,15 @@
                 </button>
               </div>
               <div class="text-right text-gray-400">
-                75/2000
+                {{ wordCount }}/2000
               </div>
             </div>
           </div>
 
           <div class="w-1/2 relative text-white bg-blue-500 rounded-xl">
-            <p class="w-full mb-10 textarea--disable-resize textarea--height p-6">Some Info</p>
+            <p class="w-full mb-10 textarea--disable-resize textarea--height p-6">
+              {{ translatedContent }}
+            </p>
             <div class="w-full absolute bottom-0 h-16 flex items-center justify-between px-6">
               <div class="w-1/2 flex items-center">
                 <button>
@@ -139,10 +146,12 @@
           </svg>
         </button>
 
-        <button class="border border-gray-300 w-12 h-12 rounded-full flex items-center justify-center text-blue-500 mx-4">
+        <button
+          class="border border-gray-300 w-12 h-12 rounded-full flex items-center justify-center text-blue-500 mx-4">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6">
             <path d="M0 0h24v24H0z" fill="none"/>
-            <path fill="currentColor" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
+            <path fill="currentColor"
+                  d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
           </svg>
         </button>
 
@@ -163,6 +172,9 @@
 <script>
   export default {
     name: 'LanguagePicker',
+    data: () => ({
+      textareaValue: ''
+    }),
     computed: {
       languages () {
         return [
@@ -173,6 +185,19 @@
           'Italian',
           'Portuguese'
         ]
+      },
+      textareClassNames () {
+        const classNames = {}
+        classNames['pointer-events-none cursor-not-allowed'] = this.wordCount >= 2000
+
+        return classNames
+      },
+      wordCount () {
+        return this.textareaValue.length
+      },
+      translatedContent() {
+        // TODO, replace with API calls to really translate the content
+        return this.textareaValue
       }
     }
   }
